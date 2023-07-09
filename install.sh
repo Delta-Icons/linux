dcaab3968038ee8de82b2c7c11b49f79cafa41cc
piuvas <piuvas@proton.me>
2023-07-09 17:10:09 -0300

    saner exiting

--- install.sh
+++ install.sh
@@ -14,44 +14,40 @@ Common_Apply () {
         sudo mkdir -p Delta /usr/share/icons
         sudo cp -r Delta /usr/share/icons
 }
-End_Program () {
-        read blegh
-        break        
-}
 if [ "$DESKTOP_SESSION" = "gnome" ] | [ "$DESKTOP_SESSION" = "xubuntu" ] | [ "$DESKTOP_SESSION" = "budgie-desktop" ] | [ "$DESKTOP_SESSION" = "pantheon" ] | [ "$DESKTOP_SESSION" = "xfce" ] ; then
         GTK_Apply
         gsettings set org.gnome.desktop.interface icon-theme "Delta"
         echo "installed"
-        End_Program
+        exit 0
 elif [ "$DESKTOP_SESSION" = "plasma" ]; then
         QT_Apply
         x=`locate plasma-changeicons`
         $x Delta
         echo "installed"
-        End_Program
+        exit 0
 elif [ "$DESKTOP_SESSION" = "cinnamon" ]; then
         read -p "Your Desktop Environment doesn't support a completely automatic install, you will have to select and apply the icon pack in your settings yourself, Proceed? y/N " proceed
         if [ "$proceed" = "y" ]; then
                 QT_Apply
         elif [ "$proceed" = "n" ]; then
-                break
+                exit 1
         fi
 else
         echo "Your Desktop Environment isn't recognised or isn't supported, We can try a few things, or you can look online for other resources,"
         read -p "Abort(0), Place in GTK non-root space(1), Place in Qt non-root space(2), Place in common space (requires root)(3)" whatdo
         if [ "$whatdo" = "0" ]; then
-                break
+                exit 1
         elif [ "$whatdo" = "1" ]; then
                 GTK_Apply                
                 echo "You should now try and apply the icon pack"
-                End_Program
+                exit 0
         elif [ "$whatdo" = "2" ]; then
                 QT_Apply
                 echo "You should now try and apply the icon pack"
-                End_Program
+                exit 0
         elif [ "$whatdo" = "3" ]; then
                 Common_Apply
                 echo "You should now try and apply the icon pack"
-                read blegh
+                exit 0
         fi
 fi

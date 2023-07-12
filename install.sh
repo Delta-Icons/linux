9e4ef8ff2b54f19b68ab1a845ba7a5826bed1d4e
derspyy <65043391+derspyy@users.noreply.github.com>
2023-07-12 13:53:28 -0300

    Improvements to install.sh (#15)

--- install.sh
+++ install.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 GTK_Apply () {
+        rm -rf ~/.icons/Delta
         mkdir -p Delta ~/.icons
-        mv Delta ~/.icons/
+        cp -r Delta ~/.icons/
 }
 QT_Apply () {
+        rm -rf ~/.local/share/icons/Delta
         mkdir -p Delta ~/.local/share/icons
-        mv Delta ~/.local/share/icons/
+        cp -r Delta ~/.local/share/icons/
 }
 Common_Apply () {
+        rm -rf /usr/share/icons/Delta
         sudo mkdir -p Delta /usr/share/icons
-        sudo mv Delta /usr/share/icons
+        sudo cp -r Delta /usr/share/icons
 }
-End_Program () {
-        read blegh
-        break        
-}
-if [ "$DESKTOP_SESSION" = "gnome" ] | [ "$DESKTOP_SESSION" = "xubuntu" ] | [ "$DESKTOP_SESSION" = "budgie-desktop" ] | [ "$DESKTOP_SESSION" = "pantheon" ] ; then
+if [ "$DESKTOP_SESSION" = "gnome" ] || [ "$DESKTOP_SESSION" = "xubuntu" ] || [ "$DESKTOP_SESSION" = "budgie-desktop" ] || [ "$DESKTOP_SESSION" = "pantheon" ] || [ "$DESKTOP_SESSION" = "xfce" ] ; then
         GTK_Apply
         gsettings set org.gnome.desktop.interface icon-theme "Delta"
         echo "installed"
-        End_Program
+        exit 0
 elif [ "$DESKTOP_SESSION" = "plasma" ]; then
         QT_Apply
-        x=`locate plasma-changeicons`
+        x=$(locate plasma-changeicons)
         $x Delta
         echo "installed"
-        End_Program
+        exit 0
 elif [ "$DESKTOP_SESSION" = "cinnamon" ]; then
         read -p "Your Desktop Environment doesn't support a completely automatic install, you will have to select and apply the icon pack in your settings yourself, Proceed? y/N " proceed
         if [ "$proceed" = "y" ]; then
                 QT_Apply
+                exit 0
         elif [ "$proceed" = "n" ]; then
-                break
+                exit 1
         fi
 else
         echo "Your Desktop Environment isn't recognised or isn't supported, We can try a few things, or you can look online for other resources,"
         read -p "Abort(0), Place in GTK non-root space(1), Place in Qt non-root space(2), Place in common space (requires root)(3)" whatdo
         if [ "$whatdo" = "0" ]; then
-                break
+                exit 1
         elif [ "$whatdo" = "1" ]; then
                 GTK_Apply                
                 echo "You should now try and apply the icon pack"
-                End_Program
+                exit 0
         elif [ "$whatdo" = "2" ]; then
                 QT_Apply
                 echo "You should now try and apply the icon pack"
-                End_Program
+                exit 0
         elif [ "$whatdo" = "3" ]; then
                 Common_Apply
                 echo "You should now try and apply the icon pack"
-                read blegh
+                exit 0
         fi
 fi
